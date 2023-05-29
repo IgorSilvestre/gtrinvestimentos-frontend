69a633be6ab69d8111f0716bcc834c813cdfba1a
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-05-29 10:33:51 -0300

    style: change header to portuguese

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,8 +1,8 @@
 <header>
 	<nav class="flex justify-center bg-black py-5">
 		<a class="no-underline hover:underline text-white mx-3" href="/">Home</a>
-		<a class="no-underline hover:underline text-white mx-3" href="/company">Companies</a>
+		<a class="no-underline hover:underline text-white mx-3" href="/company">Empresas</a>
 		<a class="no-underline hover:underline text-white mx-3" href="/tag/all">Tags</a>
-		<a class="no-underline hover:underline text-white mx-3" href="/person/646e543716ac52333c939ee2">Person</a>
+		<a class="no-underline hover:underline text-white mx-3" href="/person/646e543716ac52333c939ee2">Pessoas</a>
 	</nav>
 </header>
