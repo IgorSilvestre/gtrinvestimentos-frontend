66daba371c3ad159e9eb5c983c1772c967cee8ea
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-05-29 09:57:20 -0300

    route: change list all companies route

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,7 +1,7 @@
 <header>
 	<nav class="flex justify-center bg-black py-5">
 		<a class="no-underline hover:underline text-white mx-3" href="/">Home</a>
-		<a class="no-underline hover:underline text-white mx-3" href="/company/all">Companies</a>
+		<a class="no-underline hover:underline text-white mx-3" href="/company">Companies</a>
 		<a class="no-underline hover:underline text-white mx-3" href="/tag/all">Tags</a>
 	</nav>
 </header>
