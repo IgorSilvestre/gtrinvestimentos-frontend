033e5686c2411d08437064b3a8c3e88b59e2d451
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-07-04 19:49:13 -0300

    fix: start loding component

--- src/routes/company/+page.svelte
+++ src/routes/company/+page.svelte
@@ -1,34 +1,44 @@
 <script lang="ts">
 	import { goto } from '$app/navigation'
 	import CompanyCard from '$lib/containers/company/CompanyCard.svelte'
-  import type { ICompany } from '$lib/interfaces-validation/IVCompany.js'
+	import type { ICompany } from '$lib/interfaces-validation/IVCompany.js'
 	import TagSearch from '$lib/modules/TagSearch.svelte'
-  import { ensureArray } from '$lib/shared/functions/ensureArray'
+	import { ensureArray } from '$lib/shared/functions/ensureArray'
+	import { transitionOptions } from '$lib/shared/transitionOptions.js'
+	import { fly } from 'svelte/transition'
 
 	export let data
-
+	
 	let companyOrCompanies = data.companies || {}
 	let companies = companyOrCompanies instanceof Array ? companyOrCompanies : [companyOrCompanies]
-
-	function handleSearchCompany (event: CustomEvent<ICompany | ICompany[]>) {
-		const filteredCompanies = event.detail
-		companies = ensureArray(filteredCompanies)
+	
+	let isLoadingCompanies = companies?.length > 0 ? false : true
+	
+	function handleSearchCompany(event: CustomEvent<ICompany | ICompany[]>) {
+		companies = []
+		console.log('companies', companies)
+		isLoadingCompanies = true
+		companies = ensureArray(event.detail)
+		isLoadingCompanies = false
 	}
-
 </script>
 
 <main>
 	<div class="p-4">
-		<TagSearch on:search={handleSearchCompany} domainToFilter='company' />
+		<TagSearch on:search={handleSearchCompany} domainToFilter="company" />
 	</div>
 	<div class="flex justify-center py-5">
 		<button type="button" class="btn variant-filled-primary" on:click={() => goto('/company/new')}
 			>Criar Empresa</button
 		>
 	</div>
-	{#if companies.length > 0}
+	{#if isLoadingCompanies}
+		<div class="flex justify-center py-5">
+			<p class="text-gray-500">Loading...</p>
+		</div>
+	{:else if companies.length > 0}
 		{#each companies as company}
-			<a href={`/company/${company._id}`}>
+			<a href={`/company/${company._id}`} in:fly={transitionOptions.defaultFlyEntry}>
 				<CompanyCard {company} />
 			</a>
 		{/each}
--- src/routes/company/+page.ts
+++ src/routes/company/+page.ts
@@ -3,6 +3,7 @@ import { API_URL } from '$lib/config'
 import type { ILoadFunction } from '$lib/interfaces-validation/ILoadFunction'
 import type { ICompany } from '$lib/interfaces-validation/IVCompany'
 
+// TODO bring this to page.svelte so we can make a loading component
 export async function load({ fetch }: ILoadFunction): Promise<{ companies: ICompany }> {
 	const res: Response = await fetch(API_URL + APIEndpoints.company.getAll)
 	const data = await res.json()
