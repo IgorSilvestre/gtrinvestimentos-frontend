ea19f0b01f8d009a4c0d846031be3daa326dddfe
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-08-15 11:48:19 -0300

    style: Header - nav highlight

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,3 +1,28 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+
+	onMount(() => {
+		const links = document.querySelectorAll('nav a')
+
+		links.forEach((link) => {
+			link.addEventListener('click', (event) => {
+				// event.preventDefault() // Prevent the default link behavior
+				updateActiveLink(link as HTMLAnchorElement)
+			})
+		})
+
+		function updateActiveLink(activeLink: HTMLAnchorElement) {
+			links.forEach((link) => {
+				if (link === activeLink) {
+					link.classList.add('active')
+				} else {
+					link.classList.remove('active')
+				}
+			})
+		}
+	})
+</script>
+
 <header>
 	<nav class="flex justify-center bg-black py-5">
 		<a class="no-underline hover:underline text-white mx-3" href="/">Home</a>
