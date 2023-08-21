2ee55ac78b68c7ad63d737ad5c951f63de7b571f
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-08-21 16:14:00 -0300

    feat: copy to clipboard component

---
+++ src/lib/modules/copyToClipboardButton.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { copyToClipboard } from "$lib/shared/functions/copyToClipboard"
+
+    export let content: string;
+  </script>
+  
+  <button
+    class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
+    on:click={() => copyToClipboard(content)}
+  >
+    Copiar emails
+  </button>
