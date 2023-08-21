cfed79565d83e5d9634a6707e50f2ce32324adee
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-08-21 16:20:52 -0300

    feat: success change to copy to clipboard button

--- src/lib/modules/copyToClipboardButton.svelte
+++ src/lib/modules/copyToClipboardButton.svelte
@@ -1,12 +1,25 @@
 <script lang="ts">
-	import { copyToClipboard } from "$lib/shared/functions/copyToClipboard"
+	import { copyToClipboard } from '$lib/shared/functions/copyToClipboard'
 
-    export let content: string;
-  </script>
-  
-  <button
-    class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
-    on:click={() => copyToClipboard(content)}
-  >
-    Copiar emails
-  </button>
+	export let content: string
+
+	let copied: boolean = false
+
+	$: {
+		content, (copied = false)
+	}
+</script>
+
+{#if copied}
+	<div class="bg-green-500 hover:bg-green-700 text-white py-1 px-2 rounded">Copiado!</div>
+{:else}
+	<button
+		class="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded"
+		on:click={() => {
+			copyToClipboard(content)
+			copied = true
+		}}
+	>
+		Copiar emails
+	</button>
+{/if}
