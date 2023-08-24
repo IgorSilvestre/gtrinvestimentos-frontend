5a0c66e7eaf0ff2492215d84c27cd98eb495dd48
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-08-24 15:41:06 -0300

    chore: add toast error config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -21,3 +21,8 @@ export const toastUpdated: ToastSettings = {
 	message: 'Atualizado com sucesso!',
 	background: 'variant-filled-success'
 }
+
+export const toastError: ToastSettings = {
+	message: 'Ocorreu um erro, tente novamente mais tarde',
+	background: 'variant-filled-error'
+}
