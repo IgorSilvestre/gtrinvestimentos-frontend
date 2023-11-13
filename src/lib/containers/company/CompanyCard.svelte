57b72afe214bbf72b8e5a64c274fd23eda671d60
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-11-13 17:20:28 +0000

    style: companyCard reduced

--- src/lib/containers/company/CompanyCard.svelte
+++ src/lib/containers/company/CompanyCard.svelte
@@ -14,14 +14,12 @@
 	in:fly={transitionOptions.defaultFlyEntry}
 >
 	<div class="p-4">
-		<h2 class="text-lg font-medium text-gray-900 mb-2">{company?.name}</h2>
+		<h2 class="text-lg font-medium text-gray-900">{company?.name}</h2>
 		<div class="flex flex-wrap mb-2">
 			{#if company?.tags && company?.tags.length > 0}
 				{#each company?.tags as tag}
 					<Tag color="primary" name={tag.label} />
 				{/each}
-			{:else}
-				<p>Não foi possível carregar tags..</p>
 			{/if}
 		</div>
 		{#if company?.description}
@@ -36,41 +34,39 @@
 				{company?.target}
 			</p>
 		{/if}
-		{#if company?.employees}
-			<div class="flex flex-col">
+		{#if company?.employees && company?.employees.length > 0}
+			<div class="flex flex-col mt-1">
 				<p class="text-gray-700 text-sm">
 					<span class="font-bold">Funcionários:</span>
 				</p>
-				<div class="flex flex-wrap mb-2">
-					{#if company?.employees}
-						{#each company?.employees as employee}
-							<a href={'/person/' + employee._id}>
-								<Tag color="secondary" name={employee.name} />
-							</a>
-						{/each}
-					{:else}
-						<p>Não foi possível carregar funcionários..</p>
-					{/if}
+				<div class="flex flex-wrap">
+					{#each company?.employees as employee}
+						<a href={'/person/' + employee._id}>
+							<Tag color="secondary" name={employee.name} />
+						</a>
+					{/each}
 				</div>
 			</div>
 		{/if}
-		<div class="flex flex-col justify-end mt-4">
-			<p class="text-gray-600 text-xs">Criação: {company?.createdAt}</p>
-			<p class="text-gray-600 text-xs">Última alteração: {company?.lastUpdated}</p>
-		</div>
-		{#if !noEdit}
-			<div class="flex justify-end">
-				<button
-					type="button"
-					class="font-bold text-blue-500"
-					on:click={(e) => {
-						e.preventDefault()
-						goto('/company/edit/' + company?._id)
-					}}
-				>
-					Editar
-				</button>
+		<div class="flex justify-between">
+			<div class="flex flex-col justify-end mt-4">
+				<p class="text-gray-600 text-xs">Criação: {company?.createdAt}</p>
+				<p class="text-gray-600 text-xs">Última alteração: {company?.lastUpdated}</p>
 			</div>
-		{/if}
+			{#if !noEdit}
+				<div class="flex justify-end">
+					<button
+						type="button"
+						class="font-bold text-blue-500"
+						on:click={(e) => {
+							e.preventDefault()
+							goto('/company/edit/' + company?._id)
+						}}
+					>
+						Editar
+					</button>
+				</div>
+			{/if}
+		</div>
 	</div>
 </div>
