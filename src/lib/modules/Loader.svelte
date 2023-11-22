14fd7ff2534aa04c233784fe517fda3d96771f5a
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-11-22 17:53:28 -0300

    fix: Loader from div to span

--- src/lib/modules/Loader.svelte
+++ src/lib/modules/Loader.svelte
@@ -2,10 +2,10 @@
   export let size = 'w-12 h-12'; // default size
 </script>
 
-  <div class={`border-t-4 border-transparent border-solid rounded-full animate-spin ${size} bg-current`}></div>
+  <span class={`border-t-4 border-transparent border-solid rounded-full animate-spin ${size} bg-current`}></span>
 
 <style>
-  div {
+  span {
     border-color: currentColor;
     border-top-color: #3498db; /* Default color, can be modified */
   }
