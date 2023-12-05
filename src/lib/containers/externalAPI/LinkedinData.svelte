da526f47d52b5e9528e3d6937a23f6d983c73cb4
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-12-05 16:48:56 -0300

    Fix LinkedIn data display in company search page

--- src/lib/containers/externalAPI/LinkedinData.svelte
+++ src/lib/containers/externalAPI/LinkedinData.svelte
@@ -1,51 +1,57 @@
 <script lang="ts">
 	import type { ICompanyLinkedinData } from '$lib/interfaces-validation/ICompanyLinkedinData'
+	import { object } from 'yup'
 
 	export let data: ICompanyLinkedinData
-	const linkedinData = data.results[0]
+	const linkedinData = data && Object.keys(data).length > 1 ? data.results[0] : undefined
 </script>
 
 <main>
-	{#if linkedinData.description}
-		<p><b>Descrição: </b>{linkedinData.description}</p>
-	{/if}
-	{#if linkedinData.company_size}
-		<p><b>Tamanho aproximado: </b>{linkedinData.company_size}</p>
-	{/if}
-	{#if linkedinData.employee_count_on_li}
-		<p><b>N Funcionários Registrados: </b>{linkedinData.employee_count_on_li}</p>
-	{/if}
-	{#if linkedinData.headquarters}
-		<p><b>Localização: </b>{linkedinData.headquarters.fullAddress}</p>
-	{/if}
-	{#if linkedinData.website}
-		<p>
-			<b>Website: </b>
-			<a href={linkedinData.website} class="text-blue-400">
-				{linkedinData.website}
-			</a>
-		</p>
-	{/if}
-	<div class="mt-4">
-		{#if linkedinData.employees_on_li && linkedinData.employees_on_li.length > 0}
-			<p><b>Funcionários: </b></p>
-			{#each linkedinData.employees_on_li as employee}
-				<p class="cursor-pointer mt-2"><a href={employee.link}>{employee.name}</a></p>
-				<p class="text-sm">{employee.title}</p>
-			{/each}
+	{#if linkedinData}
+		{#if linkedinData.description}
+			<p><b>Descrição: </b>{linkedinData.description}</p>
 		{/if}
-	</div>
-	<div class="mt-4">
-		{#if linkedinData.similar_pages && linkedinData.similar_pages.length > 0}
-			<p><b>Páginas Similares</b></p>
-			{#each linkedinData.similar_pages as page}
-				<a href={page.link}>
-					<div class="flex flex-row text-center cursor-pointer mt-1">
-						<img src={page.logo} alt={page.title + ' logo'} class="w-12 mr-4" />
-						<p>{page.title}</p>
-					</div>
+		{#if linkedinData.company_size}
+			<p><b>Tamanho aproximado: </b>{linkedinData.company_size}</p>
+		{/if}
+		{#if linkedinData.employee_count_on_li}
+			<p><b>N Funcionários Registrados: </b>{linkedinData.employee_count_on_li}</p>
+		{/if}
+		{#if linkedinData.headquarters}
+			<p><b>Localização: </b>{linkedinData.headquarters.fullAddress}</p>
+		{/if}
+		{#if linkedinData.website}
+			<p>
+				<b>Website: </b>
+				<a href={linkedinData.website} class="text-blue-400">
+					{linkedinData.website}
 				</a>
-			{/each}
+			</p>
 		{/if}
-	</div>
+		<div class="mt-4">
+			{#if linkedinData.employees_on_li && linkedinData.employees_on_li.length > 0}
+				<p><b>Funcionários: </b></p>
+				{#each linkedinData.employees_on_li as employee}
+					<p class="cursor-pointer mt-2"><a href={employee.link}>{employee.name}</a></p>
+					<p class="text-sm">{employee.title}</p>
+				{/each}
+			{/if}
+		</div>
+		<div class="mt-4">
+			{#if linkedinData.similar_pages && linkedinData.similar_pages.length > 0}
+				<p><b>Páginas Similares</b></p>
+				{#each linkedinData.similar_pages as page}
+					<a href={page.link}>
+						<div class="flex flex-row text-center cursor-pointer mt-1">
+							<img src={page.logo} alt={page.title + ' logo'} class="w-12 mr-4" />
+							<p>{page.title}</p>
+						</div>
+					</a>
+				{/each}
+			{/if}
+		</div>
+	{:else}
+	<div class="mt-2 w-1/3 h-0 border border-black" />
+		<p class="mt-2"><b>Nenhuma informação do linkedin encontrada!</b></p>
+	{/if}
 </main>
--- src/routes/company-search/[companyDomain]/+page.svelte
+++ src/routes/company-search/[companyDomain]/+page.svelte
@@ -1,66 +1,55 @@
 <script lang="ts">
+	import { onMount } from 'svelte'
 	import { page } from '$app/stores'
 	import { deepSearchCompanyQuery } from '$lib/api/queries/externalAPis/deepSearchCompanyQuery'
 	import CnpjData from '$lib/containers/externalAPI/CNPJData.svelte'
 	import LinkedinData from '$lib/containers/externalAPI/LinkedinData.svelte'
-	import { VCNPJData, type ICNPJData } from '$lib/interfaces-validation/ICNPJData'
-	import {
-		VCompanyLinkedinData,
-		type ICompanyLinkedinData
-	} from '$lib/interfaces-validation/ICompanyLinkedinData'
 	import Loader from '$lib/modules/Loader.svelte'
-	import * as yup from 'yup'
+	import type { ICNPJData } from '$lib/interfaces-validation/ICNPJData'
+	import type { ICompanyLinkedinData } from '$lib/interfaces-validation/ICompanyLinkedinData'
 
 	const companyDomain = $page.params.companyDomain
-	const companyPromise = deepSearchCompanyQuery(companyDomain)
 
 	interface ICompany {
 		CNPJData: ICNPJData
 		linkedinData: ICompanyLinkedinData
 	}
 
-	const VCompany = yup.object().shape({
-		CNPJData: VCNPJData,
-		linkedinData: VCompanyLinkedinData
-	})
-
 	let company: ICompany | undefined
 	let validationError: string | undefined
+	let isLoading = true
+
+	onMount(async () => {
+		if ($page.params.companyDomain) {
+			const companyPromise = deepSearchCompanyQuery($page.params.companyDomain)
+			const fetchedCompany = await companyPromise
+			company = fetchedCompany
+			isLoading = false
+		}
+	})
 </script>
 
 <main>
-	{#await companyPromise}
+	{#if isLoading}
 		<div class="mt-10 m-auto flex justify-center items-center">
 			<Loader />
 		</div>
-	{:then fetchedCompany}
-		{(company = fetchedCompany)}
-		{#if company}
-			<script>
-				try {
-					VCompany.validateSync(company)
-				} catch (err) {
-					if (err instanceof yup.ValidationError) {
-						validationError = err.message
-					}
-				}
-			</script>
-			{#if validationError}
-				<div class="my-auto m-auto flex justify-center items-center">
-					<p class="text-2xl text-gray-600">{validationError}</p>
-				</div>
-			{:else}
-				<div class="ml-2">
-					<CnpjData data={company.CNPJData} />
-				</div>
-				<div class="ml-2">
-					<LinkedinData data={company.linkedinData} />
-				</div>
-			{/if}
-		{:else}
+	{:else if company}
+		{#if validationError}
 			<div class="my-auto m-auto flex justify-center items-center">
-				<p class="text-2xl text-gray-600">Falha ao buscar empresa..</p>
+				<p class="text-2xl text-gray-600">{validationError}</p>
+			</div>
+		{:else}
+			<div class="ml-2">
+				<CnpjData data={company.CNPJData} />
+			</div>
+			<div class="ml-2">
+				<LinkedinData data={company.linkedinData} />
 			</div>
 		{/if}
-	{/await}
+	{:else}
+		<div class="my-auto m-auto flex justify-center items-center">
+			<p class="text-2xl text-gray-600">Falha ao buscar empresa..</p>
+		</div>
+	{/if}
 </main>
