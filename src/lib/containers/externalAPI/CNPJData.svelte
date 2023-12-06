a316bddfdd06878aaa268b6b1ad8b2fb5889ba4d
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-12-05 21:21:16 -0300

    style: CNPJData.svelte component layout

--- src/lib/containers/externalAPI/CNPJData.svelte
+++ src/lib/containers/externalAPI/CNPJData.svelte
@@ -12,8 +12,8 @@
 		<div class="mt-2 w-1/3 h-0 border border-black" />
 	{:else}
 		<p><b>Nome: </b>{data.nome}</p>
-		<p><b>CNPJ: </b>{data.cnpj}</p>
-		<div>
+		<p class="mt-2"><b>CNPJ: </b>{data.cnpj}</p>
+		<div class="mt-2">
 			<p><b>Sócios: </b>{data.qsa && data.qsa.length > 0 ? '' : 'Sem sócios registrados'}</p>
 			{#if data.qsa && data.qsa.length > 0}
 				<div class="ml-4">
@@ -27,13 +27,13 @@
 			{/if}
 		</div>
 		{#if data.atividade_principal && data.atividade_principal.length > 0}
-			<div>
+			<div class="mt-2">
 				<p><b>Atividade Principal: </b></p>
 				<li class="ml-4">{data.atividade_principal[0].text}</li>
 			</div>
 		{/if}
 		{#if data.atividades_secundarias && data.atividades_secundarias.length > 0}
-			<div>
+			<div class="mt-2">
 				<p class="">
 					<b>Atividades Secundárias: </b>
 				</p>
@@ -47,21 +47,21 @@
 			</div>
 		{/if}
 		{#if data.natureza_juridica}
-			<p><b>Natureza Jurídica: </b>{data.natureza_juridica}</p>
+			<p class="mt-2"><b>Natureza Jurídica: </b>{data.natureza_juridica}</p>
 		{/if}
 		{#if data.ultima_atualizacao}
-			<p><b>Última Atualização: </b>{data.ultima_atualizacao}</p>
+			<p class="mt-2"><b>Última Atualização: </b>{data.ultima_atualizacao}</p>
 		{/if}
 		{#if data.tipo}
-			<p><b>Tipo: </b>{data.tipo}</p>
+			<p class="mt-2"><b>Tipo: </b>{data.tipo}</p>
 		{/if}
 		{#if data.data_situacao}
-			<p><b>de Abertura: </b>{data.data_situacao}</p>
+			<p class="mt-2"><b>de Abertura: </b>{data.data_situacao}</p>
 		{/if}
 		{#if data.fantasia}
-			<p><b>Nome Fantasia: </b>{data.fantasia}</p>
+			<p class="mt-2"><b>Nome Fantasia: </b>{data.fantasia}</p>
 		{/if}
-		<p>
+		<p class="mt-2">
 			<b>Endereço: </b>{data.logradouro ? `${data.logradouro},` : ''}
 			{data.numero ? `${data.numero},` : ''}
 			{data.complemento ? `${data.complemento},` : ''}
@@ -71,28 +71,25 @@
 			{data.cep ? `- ${data.cep}` : ''}
 		</p>
 		{#if data.telefone}
-			<p><b>Telefone: </b>{data.telefone}</p>
+			<p class="mt-2"><b>Telefone: </b>{data.telefone}</p>
 		{/if}
 		{#if data.email}
-			<p><b>E-mail: </b>{data.email}</p>
+			<p class="mt-2"><b>E-mail: </b>{data.email}</p>
 		{/if}
 		{#if data.situacao}
-			<p><b>Situação: </b>{data.situacao}</p>
-		{/if}
-		{#if data.data_situacao}
-			<p><b>da Situação: </b>{data.data_situacao}</p>
+			<p class="mt-2"><b>Situação: </b>{data.situacao} {data.data_situacao ? data.data_situacao : ''}</p>
 		{/if}
 		{#if data.motivo_situacao}
-			<p><b>Motivo da Situação: </b>{data.motivo_situacao}</p>
+			<p class="mt-2"><b>Motivo da Situação: </b>{data.motivo_situacao}</p>
 		{/if}
 		{#if data.situacao_especial}
-			<p><b>Situação Especial: </b>{data.situacao_especial}</p>
+			<p class="mt-2"><b>Situação Especial: </b>{data.situacao_especial}</p>
 		{/if}
 		{#if data.data_situacao_especial}
-			<p><b>da Situação Especial: </b>{data.data_situacao_especial}</p>
+			<p class="mt-2"><b>da Situação Especial: </b>{data.data_situacao_especial}</p>
 		{/if}
 		{#if data.capital_social}
-			<p><b>Capital Social: </b>{data.capital_social}</p>
+			<p class="mt-2"><b>Capital Social: </b>{data.capital_social}</p>
 		{/if}
 	{/if}
 </main>
