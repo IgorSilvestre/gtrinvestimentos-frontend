328fab00b3cdc648faa4edcd2c63817342b98b10
Igor Silvestre <igormorais.silvestre@icloud.com>
2024-02-20 19:03:19 -0300

    style: added margin to Loader component

--- src/lib/modules/Loader.svelte
+++ src/lib/modules/Loader.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-  export let size = 'w-12 h-12'; // default size
+	export let size = 'w-12 h-12' // default size
 </script>
 
-  <span class={`border-t-4 border-transparent border-solid rounded-full animate-spin ${size} bg-current`}></span>
+<span
+	class={`m-6 border-t-4 border-transparent border-solid rounded-full animate-spin ${size} bg-current`}
+/>
 
 <style>
-  span {
-    border-color: currentColor;
-    border-top-color: #3498db; /* Default color, can be modified */
-  }
+	span {
+		border-color: currentColor;
+		border-top-color: #3498db; /* Default color, can be modified */
+	}
 </style>
