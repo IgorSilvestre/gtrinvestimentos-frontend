f3dd8818a1139434fd34ce5dbdee251902658b1a
igor Silvestre <igormorais.silvestre@icloud.com>
2024-07-16 17:35:00 -0300

    refactor: ffetch business email

--- src/lib/api/queries/externalAPis/fetchBusinessEmail.ts
+++ src/lib/api/queries/externalAPis/fetchBusinessEmail.ts
@@ -1,7 +1,12 @@
 import { APIEndpoints } from '$lib/api/apiEndpoints'
 import { API } from '$lib/api/apiFetch'
 
-export async function fetchBusinessEmail(name: string, domain: string) {
+export interface IFetchBusinessEmail {
+	email: string
+	score: number
+}
+
+export async function fetchBusinessEmail(name: string, domain: string): Promise<IFetchBusinessEmail | undefined> {
 	try {
 		const { data } = await API.get(
 			APIEndpoints.externalAPI.fetchBusinessEmail + '?name=' + name + '&domain=' + domain
