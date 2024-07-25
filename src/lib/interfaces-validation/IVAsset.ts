791fe68506d1f08debaa81b5a11f5d3eab84e660
igor Silvestre <igormorais.silvestre@icloud.com>
2024-07-25 09:29:37 -0300

    feat: add attributes to IAsset interface

--- src/lib/interfaces-validation/IVAsset.ts
+++ src/lib/interfaces-validation/IVAsset.ts
@@ -2,8 +2,13 @@ import * as yup from 'yup';
 import type { IOption } from './IOption';
 
 export interface IAsset {
+    _id?: string;
     name: string;
     tags: IOption[];
+    imgURL?: string
+    environmentalAreaPercentage?: number
+    hasValuation?: boolean
+    numberOfEmployees?: number
     priceInReais?: number;
     isForSale?: boolean;
     anualRevenueInReais?: number;
@@ -16,6 +21,7 @@ export interface IAsset {
     downPaymentInReais?: number;
     description?: string;
     capRatePercentage?: number;
+    rentMonthlyInReais?: number;
     kmFromSP?: number;
     totalAreaM2?: number;
     constructedAreaM2?: number;
@@ -28,6 +34,7 @@ export interface IAsset {
     addressNumber?: string;
     addressComplement?: string;
     partner?: string;
+    contact?: string;
     zoning?: string;
 }
 
@@ -53,10 +60,15 @@ export const VAssetForm = yup.object().shape({
     downPaymentInReais: yup.number().optional(),
     description: yup.string().optional(),
     capRatePercentage: yup.number().optional(),
+    rentMonthlyInReais: yup.number().optional(),
     kmFromSP: yup.number().optional(),
     totalAreaM2: yup.number().optional(),
     constructedAreaM2: yup.number().optional(),
     vgvInReais: yup.number().optional(),
+    numberOfEmployees: yup.number().optional(),
+    environmentalAreaPercentage: yup.number().optional(),
+    hasValuation: yup.boolean().optional(),
+    imgURL: yup.string().optional(),
     docLink: yup.string().optional(),
     state: yup.string().optional(),
     city: yup.string().optional(),
@@ -65,6 +77,7 @@ export const VAssetForm = yup.object().shape({
     addressNumber: yup.string().optional(),
     addressComplement: yup.string().optional(),
     partner: yup.string().optional(),
+    contact: yup.string().required(),
     zoning: yup.string().optional(),
 })
 
