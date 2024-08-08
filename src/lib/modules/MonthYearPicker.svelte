aa5d283f9e3681e760a2c5df3ad25201bf7fc8d3
igor Silvestre <igormorais.silvestre@icloud.com>
2024-08-08 11:39:42 -0300

    feat: component: MonthDatePicker.svelte

---
+++ src/lib/modules/MonthYearPicker.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let date = undefined;
+
+  function handleChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const selectedDate = target.value;
+    try {
+      date = new Date(selectedDate).toISOString()
+    } catch (error) {
+      date = undefined
+    }
+  }
+</script>
+
+<div class="month-picker">
+  <input
+    type="month"
+    id="month"
+    on:change={handleChange}
+    class="mt-1 block w-full bg-gray-100 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+  />
+</div>
+
