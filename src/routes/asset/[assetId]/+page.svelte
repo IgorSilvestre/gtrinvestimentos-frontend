ea11069cb3ec55b6ae8ca358b9555045d875f3c7
igor Silvestre <igormorais.silvestre@icloud.com>
2024-08-09 17:33:07 -0300

    feat: Asset ID page

---
+++ src/routes/asset/[assetId]/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import AssetFullCard from '$lib/containers/asset/AssetFullCard.svelte'
+
+	export let data
+	const { asset } = data
+</script>
+
+<main>
+	<AssetFullCard {asset} />
+</main>
---
+++ src/routes/asset/[assetId]/+page.ts
@@ -0,0 +1,15 @@
+import { APIEndpoints } from '$lib/api/apiEndpoints';
+import { API_URL } from '$lib/config';
+import type { ILoadFunction } from '$lib/interfaces-validation/ILoadFunction';
+import type { IOption } from '$lib/interfaces-validation/IOption';
+import type { IAsset } from '$lib/interfaces-validation/IVAsset';
+
+export async function load({ fetch, params }: ILoadFunction): Promise<{ asset: IAsset, tags: IOption[] }> {
+    const assetRes = await fetch(`${API_URL}${APIEndpoints.asset.getById}${params.assetId}`);
+    const [ asset ]: IAsset = await assetRes.json();  // Assuming the API returns a single asset object
+
+    const tagsRes: Response = await fetch(`${API_URL}${APIEndpoints.tags.getAllForSelect}`);
+    const tags: IOption[] = await tagsRes.json();
+
+    return { asset, tags };
+}
