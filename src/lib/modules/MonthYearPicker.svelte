53084f9cd0263b90b4d21d016023f52aa5559e02
igor Silvestre <igormorais.silvestre@icloud.com>
2024-08-15 11:46:26 -0300

    fix: MonthYearPicker.svelte : fill component when edit

--- src/lib/modules/MonthYearPicker.svelte
+++ src/lib/modules/MonthYearPicker.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
-  export let date: string | undefined= undefined;
+  export let date: string | undefined = undefined;
+
+  // Format the incoming date to YYYY-MM if it's defined
+  let formattedDate: string | undefined = date ? date.slice(0, 7) : undefined;
 
   function handleChange(event: Event) {
     const target = event.target as HTMLInputElement;
     const selectedDate = target.value;
+
     try {
-      date = new Date(selectedDate).toISOString()
+      // Convert the selected date to ISO format
+      date = new Date(`${selectedDate}-01`).toISOString();
     } catch (error) {
-      date = undefined
+      date = undefined;
     }
   }
 </script>
@@ -18,6 +23,7 @@
     id="month"
     on:change={handleChange}
     class="mt-1 block w-full bg-gray-100 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+    bind:value={formattedDate}
   />
 </div>
 
