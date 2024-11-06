28baf7b134d8e2bf1ecb42e41c7feeedb7e1ff7e
igor Silvestre <igor@gms.capital>
2024-11-06 13:42:30 -0300

    fix: get company by id endpoint

--- src/routes/company/[companyId]/+page.ts
+++ src/routes/company/[companyId]/+page.ts
@@ -5,7 +5,7 @@ import type { ICompany } from '$lib/interfaces-validation/IVCompany'
 
 
 export async function load({ fetch, params }: ILoadFunction): Promise<{ company: ICompany, tags: IOption[] }> {
-	const companyRes = await fetch(API_URL + 'company/' + params.companyId)
+	const companyRes = await fetch(API_URL + 'v1/company/' + params.companyId)
 	const [ company ] = await companyRes.json()
 	const tagsRes: Response = await fetch(API_URL + 'tag/all-for-select')
 	const tags = await tagsRes.json()
