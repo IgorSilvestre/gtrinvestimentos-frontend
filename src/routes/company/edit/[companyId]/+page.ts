cf94223730bc788052fbc57815d776c265784f3b
igor Silvestre <igor@gms.capital>
2024-12-05 10:21:50 -0300

    fix: company edit endpoint

--- src/routes/company/edit/[companyId]/+page.ts
+++ src/routes/company/edit/[companyId]/+page.ts
@@ -1,9 +1,10 @@
+import { APIEndpoints } from '$lib/api/apiEndpoints'
 import { API_URL } from '$lib/config'
 import type { ILoadFunction } from '$lib/interfaces-validation/ILoadFunction'
 import type { ICompany } from '$lib/interfaces-validation/IVCompany'
 
 export async function load({ fetch, params }: ILoadFunction): Promise<{ company: ICompany }> {
-	const res: Response = await fetch(API_URL + 'company/' + params.companyId)
+	const res: Response = await fetch(API_URL + APIEndpoints.company.url_v1 + params.companyId)
 	const data: ICompany[] = await res.json()
 	return { company: data[0] }
 }
