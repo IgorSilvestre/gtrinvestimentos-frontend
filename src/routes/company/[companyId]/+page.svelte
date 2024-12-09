6572e0d0409fa5ee3dbf94c90e4494ba94ee41f7
igor Silvestre <igor@gms.capital>
2024-12-09 17:41:08 -0300

    fix: auth on [companyId] page

--- src/routes/company/[companyId]/+page.svelte
+++ src/routes/company/[companyId]/+page.svelte
@@ -1,35 +1,57 @@
 <script lang="ts">
+	import { APIEndpoints } from '$lib/api/apiEndpoints.js'
+	import { API } from '$lib/api/apiFetch.js'
 	import CompanyCard from '$lib/containers/company/CompanyCard.svelte'
 	import PersonCard from '$lib/containers/person/PersonCard.svelte'
 	import type { IOption } from '$lib/interfaces-validation/IOption.js'
-	import type { IEmployee } from '$lib/interfaces-validation/IVCompany.js'
-	import type { IPerson } from '$lib/interfaces-validation/IVPerson.js'
+	import Loader from '$lib/modules/Loader.svelte'
+	import { onMount } from 'svelte'
 
 	export let data
-	const { company, tags } = data
-	const employees: IEmployee[] = company.employees || []
-	const employeesParsed: IPerson[] = []
-	for (const person of employees) {
-		const employeeTags: IOption[] = person.tags?.flatMap((tagId: string) =>
-			tags.filter((tag) => tag.value === tagId)
-		)
-		employeesParsed.push({
-			...person,
-			tags: employeeTags,
-			company
-		})
-	}
+
+	let isLoading = false
+	let company: any
+	let tags: any
+	let employeesParsed: any
+	let employees: any
+	onMount(async () => {
+		isLoading = true
+		const companyResponse = await API.get(APIEndpoints.company.url_v1 + data.companyId)
+		company = await companyResponse.data[0]
+
+		const tagsResponse = await API.get(APIEndpoints.tags.getAllForSelect)
+		tags = await tagsResponse.data
+
+		employees = company?.employees || []
+		employeesParsed = []
+		for (const person of employees) {
+			const employeeTags: IOption[] = person?.tags?.flatMap((tagId: string) =>
+				tags?.filter((tag: any) => tag.value === tagId)
+			)
+			employeesParsed.push({
+				...person,
+				tags: employeeTags,
+				company
+			})
+		}
+		isLoading = false
+	})
 </script>
 
 <main>
-	<CompanyCard {company} />
-
-	{#if employees.length > 0}
-		<h2 class="text-lg font-medium text-gray-900 mb-2 mx-4">Funcionários:</h2>
-		{#each employeesParsed as person}
-			<a href="/person/{person._id}">
-				<PersonCard {person} noEdit />
-			</a>
-		{/each}
+	{#if isLoading}
+		<div class="flex justify-center mx-4 my-6">
+			<Loader />
+		</div>
+	{:else}
+		<CompanyCard {company} />
+		{#if employees?.length > 0}
+			<h2 class="text-lg font-medium text-gray-900 mb-2 mx-4">Funcionários:</h2>
+			{#each employeesParsed as person}
+				<a href="/person/{person._id}">
+					<PersonCard {person} noEdit />
+				</a>
+			{/each}
+		{/if}
 	{/if}
 </main>
